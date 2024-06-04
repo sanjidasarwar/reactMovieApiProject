55e7f368f449b3fc1b5cc39762b4900de9d7ae7b
tupsi07 <sanjida657@yahoo.com>
2024-06-05 00:06:15 +0600

    header section

---
+++ src/components/Header.jsx
@@ -0,0 +1,136 @@
+function Header() {
+  return (
+    <header className="py-4 bg-gray-900">
+      <div className="md:container md:mx-auto">
+        <div className="flex justify-between">
+          <div>
+            <h3 className="text-[#f40b7e] text-2xl">Filmix</h3>
+          </div>
+          <div className="flex">
+            <form>
+              <div className="flex">
+                <button
+                  id="dropdown-button"
+                  data-dropdown-toggle="dropdown"
+                  className="flex-shrink-0 z-10 inline-flex items-center py-1.5 px-2 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
+                  type="button"
+                >
+                  All
+                  <svg
+                    className="w-2.5 h-2.5 ms-2.5"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 10 6"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="m1 1 4 4 4-4"
+                    />
+                  </svg>
+                </button>
+                <div
+                  id="dropdown"
+                  className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+                >
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                    aria-labelledby="dropdown-button"
+                  >
+                    <li>
+                      <button
+                        type="button"
+                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Movies
+                      </button>
+                    </li>
+                    <li>
+                      <button
+                        type="button"
+                        className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        TV
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+                <div className="relative w-full">
+                  <input
+                    type="search"
+                    id="search-dropdown"
+                    className="block px-2.5 py-1.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                    required
+                  />
+                  <button
+                    type="submit"
+                    className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-dark rounded-e-lg focus:ring-4 focus:outline-none  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  >
+                    <svg
+                      className="w-4 h-4"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 20 20"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                      />
+                    </svg>
+                    <span className="sr-only">Search</span>
+                  </button>
+                </div>
+              </div>
+            </form>
+            <button
+              data-tooltip-target="navbar-search-example-toggle-dark-mode-tooltip"
+              type="button"
+              data-toggle-dark="light"
+              className="flex items-center p-2 ml-2 text-xs font-medium text-gray-700 bg-white rounded-lg border border-gray-200 toggle-dark-state-example hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-gray-300 dark:focus:ring-gray-500 dark:bg-gray-800 focus:outline-none dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+            >
+              <svg
+                aria-hidden="true"
+                data-toggle-icon="sun"
+                className="w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <svg
+                aria-hidden="true"
+                data-toggle-icon="moon"
+                className="w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+              </svg>
+            </button>
+          </div>
+        </div>
+        <div className="text-white flex justify-end pt-5">
+          <a href="" className="px-1.5  hover:text-[#f40b7e] transition-colors duration-700">Home</a>
+          <a href="" className="px-1.5 pl-7  hover:text-[#f40b7e] transition-colors duration-700">Movie</a>
+          <a href="" className="px-1.5 pl-7  hover:text-[#f40b7e] transition-colors duration-700">TV</a>
+          <a href="" className="px-1.5 pl-7  hover:text-[#f40b7e] transition-colors duration-700">Celebrities</a>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
